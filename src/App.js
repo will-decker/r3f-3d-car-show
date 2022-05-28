ca2fdcad8004bcdf89bb20864b90d01fd594afff
willdecker <willdecker@gmail.com>
2022-05-28 12:36:01 -0400

    complete ground component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import './style.css';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Ground } from './Ground';
 
 function CarShow() {
   return (
@@ -38,6 +39,8 @@ function CarShow() {
         castShadow
         shadow-bias={-0.0001}
       />
+
+      <Ground />
     </>
   );
 }
--- src/Ground.js
+++ src/Ground.js
@@ -1,13 +1,33 @@
+import React, { useEffect } from 'react';
 import { MeshReflectorMaterial } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import { LinearEncoding, RepeatWrapping, TextureLoader } from 'three';
 
 export function Ground() {
+  const [roughness, normal] = useLoader(TextureLoader, [
+    process.env.PUBLIC_URL + 'textures/terrain-roughness.jpeg',
+    process.env.PUBLIC_URL + 'textures/terrain-normal.jpeg',
+  ]);
+
+  useEffect(() => {
+    [normal, roughness].forEach((t) => {
+      t.wrapS = RepeatWrapping;
+      t.wrapT = RepeatWrapping;
+      t.repeat.set(5, 5);
+    });
+
+    normal.encoding = LinearEncoding;
+  }, [normal, roughness]);
+
   // Three.js: mesh.rotation.x = -Math.PI * 0.5
   return (
     <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
       <planeGeometry args={[30, 30]} />
       <MeshReflectorMaterial
         envMapIntensity={0}
-        roughnessMap={0.7}
+        normalMap={normal}
+        normalScale={[0.15, 0.15]}
+        roughnessMap={roughness}
         dithering={true}
         color={[0.015, 0.015, 0.015]}
         roughness={0.7}
