3b8f3047031b00ec464d3a2bc35d02b8393870d7
willdecker <willdecker@gmail.com>
2022-05-30 23:05:52 -0400

    animate car wheels

--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useLoader } from '@react-three/fiber';
+import { useFrame, useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Mesh } from 'three';
 
@@ -20,6 +20,16 @@ export function Car() {
       }
     });
   }, [gltf]);
+
+  useFrame((state, delta) => {
+    let t = state.clock.getElapsedTime() * 2;
+
+    let group = gltf.scene.children[0].children[0].children[0];
+    group.children[0].rotation.x = t;
+    group.children[2].rotation.x = t;
+    group.children[4].rotation.x = t;
+    group.children[6].rotation.x = t;
+  });
 
   return <primitive object={gltf.scene} />;
 }
